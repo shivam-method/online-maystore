b8e0639829dce008b038220ac5d43027366dbe5e
Shivam Kumar <shivam-method@users.noreply.github.com>
2020-05-02 22:00:23 +0530

    Initial commit after project and configuration setup

---
+++ maystore/src/main/java/com/shivamone/maystore/MaystoreApplication.java
@@ -0,0 +1,13 @@
+package com.shivamone.maystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaystoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaystoreApplication.class, args);
+	}
+
+}
---
+++ maystore/src/test/java/com/shivamone/maystore/MaystoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivamone.maystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaystoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
